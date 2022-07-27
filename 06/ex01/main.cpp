557500d925348e57e973759af8159d85ad07c97a
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2022-07-27 18:28:29 +0200

    add ex02 wip

--- 06/ex01/main.cpp
+++ 06/ex01/main.cpp
@@ -6,14 +6,22 @@
 /*   By: cjulienn <cjulienn@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 16:11:41 by cjulienn          #+#    #+#             */
-/*   Updated: 2022/07/27 17:13:29 by cjulienn         ###   ########.fr       */
+/*   Updated: 2022/07/27 17:21:17 by cjulienn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Data.hpp"
 #include <iostream>
 
-int main(int argc, char **argv)
+/*
+
+IMPORTANT THEORETICAL POINT : the typedef uintptr_t pointer
+assure the user that if you cast it from a pointer of a given object and
+recast it after it wont change the pointer value or alter the memory
+
+*/
+
+int main(void)
 {
 	Data			*example = new Data;
 	uintptr_t		compression;
@@ -31,16 +39,16 @@ int main(int argc, char **argv)
 	// verificating that the values are actually the same
 	std::cout << "Original values are : " << std::endl;
 	std::cout << "example->name = " << example->name << std::endl;
-	std::cout << "example->random_num = " << example->name << std::endl;
-	std::cout << "example->random_float = " << example->name << std::endl;
-	std::cout << "example->random_char = " << example->name << std::endl;
+	std::cout << "example->random_num = " << example->random_num << std::endl;
+	std::cout << "example->random_float = " << example->random_float << std::endl;
+	std::cout << "example->random_char = " << example->random_char << std::endl;
 
 	std::cout << "---------------------------------------------" << std::endl;
 	std::cout << "New values are : " << std::endl;
-	std::cout << "recovery->name = " << example->name << std::endl;
-	std::cout << "recovery->random_num = " << example->name << std::endl;
-	std::cout << "recovery->random_float = " << example->name << std::endl;
-	std::cout << "recovery->random_char = " << example->name << std::endl;
+	std::cout << "recovery->name = " << recovery->name << std::endl;
+	std::cout << "recovery->random_num = " << recovery->random_num << std::endl;
+	std::cout << "recovery->random_float = " << recovery->random_float << std::endl;
+	std::cout << "recovery->random_char = " << recovery->random_char << std::endl;
 
 	// verification of pointer values
 	std::cout << "---------------------------------------------" << std::endl;
@@ -49,6 +57,5 @@ int main(int argc, char **argv)
 
 	// freeing memory
 	delete example;
-	delete recovery; // double free ?
 	return (0);
 }
