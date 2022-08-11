7917d74b55ca2227865ee9056d4d29604c648214
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2022-08-11 12:11:33 +0200

    day 01 ex04

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cjulienn <cjulienn@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 11:30:14 by cjulienn          #+#    #+#             */
-/*   Updated: 2022/08/10 17:50:05 by cjulienn         ###   ########.fr       */
+/*   Updated: 2022/08/11 12:10:43 by cjulienn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,32 @@
 #include <fstream>
 #include <string>
 
-void	testing_procedure()
-{
-	// TO IMPLEMENT
-}
-
 /* ifstream is used to open a file within it to be able to manipulated it later 
 */
 
-int	main(int argc, char **argv)
+std::string	str_ops(std::string gnl, std::string s1, std::string s2)
 {
-	std::string			gnl;
-	std::string 		name_new_file(argv[1]);
-	std::ofstream		new_file(name_new_file.append(".replace"));
+	std::string		new_gnl(gnl);
+	size_t			needle_pos;
+
+	while (42)
+	{
+		needle_pos = new_gnl.find(s1);
+		if (needle_pos == std::string::npos)
+			break ;
+		else
+		{
+			new_gnl.erase(needle_pos, s1.size());
+			new_gnl.insert(needle_pos, s2);
+		}
+	}
+	return (new_gnl);
+}
+
+int	file_ops(char **argv)
+{
 	std::ifstream		file;
 
-	if (argc != 4)
-	{
-		std::cout << "Error: Wrong number of arguments" << std::endl;
-		return (1);
-	}
 	file.open(argv[1], std::ifstream::in);
 	if (file.good() == false)
 	{
@@ -46,17 +52,30 @@ int	main(int argc, char **argv)
 		file.close();
 		return (1);
 	}
-	while (42)
-	{
-		// std::size_t		index;
 	
-		std::getline(file, gnl, '\n');
-		std::cout << gnl << std::endl; // test
-		if (gnl.empty())
-			break ;
+	std::string			gnl;
+	std::string			name_file(argv[1]);
+	std::ofstream		new_file(name_file.append(".replace"));
+
+	std::string			s1(argv[2]);
+	std::string			s2(argv[3]);
+
+	while (std::getline(file, gnl))
+	{
+		gnl = str_ops(gnl, s1, s2);
 		new_file << gnl << std::endl;
 	}
 	file.close(); // close the original file
 	new_file.close(); // close the newly created file
 	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Error: Wrong number of arguments" << std::endl;
+		return (1);
+	}
+	return (file_ops(argv));
 }
